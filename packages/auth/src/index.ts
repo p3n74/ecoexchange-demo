fc509e2d6c9083f6afe9c76ce402dc3afec07adf
pentuhhh <firefall.challenger@gmail.com>
2026-05-03 23:17:34 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@ecoExchange/db";
+import * as schema from "@ecoExchange/db/schema/auth";
+import { env } from "@ecoExchange/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "sqlite",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
